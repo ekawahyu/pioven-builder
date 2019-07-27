6ae454b5d6c6e2c2e46b7896569235f5e7495110
Ekawahyu Susilo <ekawahyu@yahoo.com>
2019-07-26 17:35:15 -0700

    added pibuilder and first run script to build an wireless AP gateway

---
+++ scripts/first_run_gateway.sh
@@ -0,0 +1,191 @@
+#!/bin/sh
+
+exec 2>> /var/log/rc.local.log # send stderr from rc.local to a log file
+exec 1>&2
+
+set -x
+set -e
+
+DATA_DIR=/opt/data
+
+stage_one(){
+#
+# Install SSH key
+#
+install -d -m 700 /home/pi/.ssh
+mv /id_rsa.pub /home/pi/.ssh/authorized_keys
+chown pi:pi -Rf /home/pi/.ssh/
+#
+# Modify sources.list to point to US mirror
+#
+sudo sed -e '/deb.*/ s/^#*/#/' -i /etc/apt/sources.list
+sudo bash -c 'cat >> /etc/apt/sources.list << EOL
+deb http://mirrors.ocf.berkeley.edu/raspbian/raspbian/ stretch main contrib non-free rpi
+EOL'
+sudo sed -e '/deb.*/ s/^#*/#/' -i /etc/apt/sources.list.d/raspi.list
+sudo bash -c 'cat >> /etc/apt/sources.list.d/raspi.list << EOL
+deb http://mirrors.ocf.berkeley.edu/rasppi/debian/ stretch main ui
+EOL'
+sudo apt-get update
+sudo apt-get -y upgrade
+}
+
+stage_two(){
+#
+# Install hostapd and dnsmasq
+#
+sudo apt-get install -y hostapd dnsmasq
+sudo systemctl stop hostapd
+sudo systemctl stop dnsmasq
+sudo bash -c 'cat >> /etc/dhcpcd.conf << EOL
+interface wlan0
+	static ip_address=192.168.220.1/24
+	nohook wpa_supplicant
+EOL'
+# sudo systemctl restart dhcpcd
+sudo touch /etc/hostapd/hostapd.conf
+sudo chmod g+w /etc/hostapd/hostapd.conf
+sudo bash -c 'cat > /etc/hostapd/hostapd.conf << EOL
+interface=wlan0
+driver=nl80211
+
+hw_mode=g
+channel=6
+ieee80211n=1
+wmm_enabled=0
+macaddr_acl=0
+ignore_broadcast_ssid=0
+
+auth_algs=1
+wpa=2
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
+
+# This is the name of the network
+ssid=gateway
+# The network passphrase
+wpa_passphrase=password
+EOL'
+#
+# Assign ssid=gateway-xxxx and password=c0nectr1c
+#
+sudo sed -i '/^ssid=/s/.*/'"$(sed 's/://g' /sys/class/net/wlan0/address | sed 's/.*\(....\)$/\1/' | sed 's/.*/ssid=gateway-&/')"'/' /etc/hostapd/hostapd.conf
+sudo sed -i '/^wpa_passphrase=/s/.*/wpa_passphrase=c0nectr1c/' /etc/hostapd/hostapd.conf
+sudo sed -i '/127.0.1.1\traspberrypi/s/.*/'"$(sed 's/://g' /sys/class/net/wlan0/address | sed 's/.*\(....\)$/\1/' | sed 's/.*/127.0.1.1\tgateway-&/')"'/' /etc/hosts
+sed 's/://g' /sys/class/net/wlan0/address | sed 's/.*\(....\)$/\1/' | sed 's/.*/gateway-&/' | sudo tee /etc/hostname > /dev/null 2>&1
+#
+# Configure hostpad and port forwarding
+#
+sudo sed -i 's/#DAEMON_CONF=.*/DAEMON_CONF=\"\/etc\/hostapd\/hostapd.conf\"/g' /etc/default/hostapd
+sudo sed -i 's/DAEMON_CONF=.*/DAEMON_CONF=\/etc\/hostapd\/hostapd.conf/g' /etc/init.d/hostapd
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+sudo bash -c 'cat > /etc/dnsmasq.conf << EOL
+interface=wlan0      # Use interface wlan0  
+server=1.1.1.1       # Use Cloudflare DNS  
+dhcp-range=192.168.220.50,192.168.220.150,12h # IP range and lease time
+EOL'
+sudo sed -i 's/#net.ipv4.ip_forward=.*/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sudo bash -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo bash -c 'iptables-save > /etc/iptables.ipv4.nat'
+sudo sed '/exit 0/i iptables-restore < \/etc\/iptables.ipv4.nat\n' /etc/rc.local
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo systemctl start hostapd
+sudo service dnsmasq start
+}
+
+if [ -f /var/log/rebooting-done ]; then
+sudo rm /var/log/rebooting-done
+sudo sed -i '\/first_run_gateway.sh/d' /etc/rc.local
+sudo rm -Rf ${DATA_DIR}
+sudo rm -- "$0"
+echo 'Deleted current script and rebooting!'
+sudo reboot
+elif [ -f /var/log/rebooting-to-stage-2 ]; then
+stage_two
+sudo rm /var/log/rebooting-to-stage-2
+sudo touch /var/log/rebooting-done
+sudo reboot
+else
+stage_one
+sudo touch /var/log/rebooting-to-stage-2
+sudo reboot
+fi
+
+# Delete "pi" user and create another one
+#useradd -m %PI_USERNAME% -G sudo || true
+#echo "%PI_USERNAME%:%PI_PASSWORD%" | chpasswd
+#install -d -m 700 /home/%PI_USERNAME%/.ssh
+#mv /id_rsa.pub /home/%PI_USERNAME%/.ssh/authorized_keys
+#chown %PI_USERNAME%:%PI_USERNAME% -Rf /home/%PI_USERNAME%/.ssh/
+
+#echo "%PI_USERNAME% ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/010_%PI_USERNAME%-nopasswd
+
+#rm /etc/sudoers.d/010_pi-nopasswd
+#deluser -remove-home pi
+
+# Change user and group ID
+#usermod -u 1000 --shell /bin/bash %PI_USERNAME%
+#groupmod -g 1000 %PI_USERNAME%
+
+# Configure hostname
+#randomWord1=$(shuf ${DATA_DIR}/words.txt -n 1 | sed -e "s/\s/-/g")
+#randomWord2=$(shuf ${DATA_DIR}/words.txt -n 1 | sed -e "s/\s/-/g")
+#PI_CONFIG_HOSTNAME="%PI_HOSTNAME%-${randomWord1}-${randomWord2}"
+
+#echo "${PI_CONFIG_HOSTNAME}" > "/etc/hostname"
+#OLD_HOST="raspberrypi"
+#sed -i "s/$OLD_HOST/$PI_CONFIG_HOSTNAME/g" "/etc/hosts"
+#hostnamectl set-hostname "${PI_CONFIG_HOSTNAME}"
+
+# Configure the memory split
+#if test "%PI_GPU_MEMORY%" = "16" || test "%PI_GPU_MEMORY%" = "32" || test "%PI_GPU_MEMORY%" = "64" || test "%PI_GPU_MEMORY%" = "128" || test "%PI_GPU_MEMORY%" = "256"; then
+#  echo "gpu_mem=%PI_GPU_MEMORY%" >> /boot/config.txt
+#fi
+
+# Configure static IP address
+#apt-get -qq update
+#apt-get install -y python-dev python-pip
+#pip install netifaces
+
+#export TARGET_IP="target_ip"
+#export NETWORK_CONFIG="/etc/network/interfaces"
+#export PI_IP_ADDRESS_RANGE_START="%PI_IP_ADDRESS_RANGE_START%"
+#export PI_IP_ADDRESS_RANGE_END="%PI_IP_ADDRESS_RANGE_END%"
+#export PI_DNS_ADDRESS="%PI_DNS_ADDRESS%"
+#python /interfaces.py
+
+#cat /etc/network/interfaces
+#rm /interfaces.py
+#pip uninstall -y netifaces
+#apt-get remove -y python-dev python-pip
+
+#PI_IP_ADDRESS=$(cat ./target_ip)
+#rm ./target_ip
+
+# Remove DHCPCD5 - https://www.raspberrypi.org/forums/viewtopic.php?t=111709
+#apt-get remove -y dhcpcd5
+
+# Install Docker
+#if "%PI_INSTALL_DOCKER%" -eq "true"; then
+#  curl -sSL https://get.docker.com | CHANNEL=stable sh
+#  usermod -aG docker %PI_USERNAME%
+#fi
+
+# Send email telling about this server
+#if test "%PI_MAILGUN_API_KEY%" && test "%PI_MAILGUN_DOMAIN%" && test "%PI_EMAIL_ADDRESS%"; then
+#  curl -s --user "api:%PI_MAILGUN_API_KEY%" \
+#    https://api.mailgun.net/v3/%PI_MAILGUN_DOMAIN%/messages \
+#    -F from="%PI_USERNAME%@%PI_MAILGUN_DOMAIN%" \
+#    -F to=%PI_EMAIL_ADDRESS% \
+#    -F subject="New Raspberry Pi (${PI_CONFIG_HOSTNAME}) set up" \
+#    -F text="New %PI_USERNAME%@${PI_CONFIG_HOSTNAME} setup on: ${PI_IP_ADDRESS}"
+#fi
+
+#rm -Rf ${DATA_DIR}
+
+#rm -- "$0"
+
+#echo "Deleted current script"
---
+++ scripts/pibuilder_gateway.sh
@@ -0,0 +1,158 @@
+#!/bin/sh
+
+set -e
+
+CACHE_DIR="./cache"
+RESULT_FILE="${CACHE_DIR}/result"
+SETTINGS_FILE="./settings.sh"
+BOOT_DIR="/media/rpi_boot"
+ROOT_DIR="/media/rpi_root"
+UNZIP_TARGET="${CACHE_DIR}/os.img"
+
+download () {
+  # Add an MD5 sum on the filename to ensure we have the correct file
+  SRC=${1}
+  SUM=$(printf '%s' $SRC | md5sum | cut -d ' ' -f 1)
+
+  mkdir -p ${CACHE_DIR}
+
+  TARGET="${CACHE_DIR}/os.${SUM}.zip"
+
+  if [ ! -f "${TARGET}" ]; then
+    curl -L ${SRC} -o ${TARGET}
+  fi
+
+  rm -f ${UNZIP_TARGET}
+  unzip -o ${TARGET} -d "${CACHE_DIR}/unzip"
+  mv ${CACHE_DIR}/unzip/* ${UNZIP_TARGET}
+  rm -Rf "${CACHE_DIR}/unzip"
+
+  sleep 1
+}
+
+padNumber () {
+  NUMBER=${1}
+  printf "%03d\n" ${NUMBER}
+}
+
+if [ $UID != 0 ]; then
+  echo 'Script must be run as root'
+  exit 1
+fi
+
+rm -f ${RESULT_FILE}
+
+# Create directories to mount the images to
+mkdir -p ${BOOT_DIR} ${ROOT_DIR}
+
+if [ ! -f "${SETTINGS_FILE}" ]; then
+  # Running setup first
+  echo "Please add configuration to ${SETTINGS_FILE}"
+  exit 1
+fi
+
+# Load up the variables
+source ${SETTINGS_FILE}
+
+# Validate the settings
+if [[ -z "${PI_HOSTNAME}" ]]; then
+  echo "PI_HOSTNAME needs to be configured with your desired hostname"
+  exit 1
+fi
+
+if [[ -z "${PI_SSH_KEY}" ]]; then
+  echo "PI_SSH_KEY needs to be configured with your desired SSH key"
+  exit 1
+fi
+
+if ! [[ -f "${PI_SSH_KEY}" ]]; then
+  echo "PI_SSH_KEY (${PI_SSH_KEY}) is not a file"
+  exit 1
+fi
+
+#if [[ -z "${PI_USERNAME}" ]]; then
+#  echo "PI_USERNAME needs to be configured with your designed username"
+#  exit 1
+#fi
+
+# Generate the password
+PI_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${PI_PASSWORD_LENGTH} | head -n 1)
+
+# Download the operating system
+download ${PI_OS}
+
+LOOP=$(kpartx -sva ${UNZIP_TARGET})
+
+MOUNT_POINTS=$(echo ${LOOP} | grep -ioE 'loop(\w+)')
+
+umount ${BOOT_DIR} || true
+umount ${ROOT_DIR} || true
+
+while read -r line; do
+  if [[ $line == *"p1" ]]; then
+    mount "/dev/mapper/${line}" ${BOOT_DIR} -o ro --rw
+  elif [[ $line == *"p2" ]]; then
+    mount "/dev/mapper/${line}" ${ROOT_DIR} -o ro --rw
+  fi
+done <<< "$MOUNT_POINTS"
+
+sleep 1
+
+# Enable SSH
+touch ${BOOT_DIR}/ssh
+
+# Configure WiFi - if required
+if [[ "${PI_WIFI_SSID}" && "${PI_WIFI_PASS}" ]]; then
+  if ! (grep -q "ssid=" "${ROOT_DIR}/etc/wpa_supplicant/wpa_supplicant.conf"); then
+    printf "\nnetwork={\n  ssid=\"%s\"\n  psk=\"%s\"\n}\n" ${PI_WIFI_SSID} ${PI_WIFI_PASS} >> ${ROOT_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
+  fi
+fi
+
+# Set the first boot script
+if ! (grep -q "./scripts/first_run_gateway.sh" "${ROOT_DIR}/etc/rc.local"); then
+#  sed -i '$ d' "${ROOT_DIR}/etc/rc.local"
+#  printf "if [ -f /first_run_gateway.sh ]; then\n  printf 'Setting up the Pi'\n  sh /first_run_gateway.sh\n  reboot\n\n  printf 'Pi setup'\nfi\n\nexit 0\n" >> "${ROOT_DIR}/etc/rc.local"
+sudo sed -i '/exit 0/i \/first_run_gateway.sh\n' "${ROOT_DIR}/etc/rc.local"
+fi
+
+cp ./scripts/first_run_gateway.sh "${ROOT_DIR}/first_run_gateway.sh"
+sed -i "s/%PI_HOSTNAME%/$PI_HOSTNAME/g" "${ROOT_DIR}/first_run_gateway.sh"
+sed -i "s/%PI_USERNAME%/$PI_USERNAME/g" "${ROOT_DIR}/first_run_gateway.sh"
+sed -i "s/%PI_PASSWORD%/$PI_PASSWORD/g" "${ROOT_DIR}/first_run_gateway.sh"
+sed -i "s/%PI_WIFI_SSID%/$PI_WIFI_SSID/g" "${ROOT_DIR}/first_run_gateway.sh"
+sed -i "s/%PI_MAILGUN_API_KEY%/$PI_MAILGUN_API_KEY/g" "${ROOT_DIR}/first_run_gateway.sh"
+sed -i "s/%PI_MAILGUN_DOMAIN%/$PI_MAILGUN_DOMAIN/g" "${ROOT_DIR}/first_run_gateway.sh"
+sed -i "s/%PI_EMAIL_ADDRESS%/$PI_EMAIL_ADDRESS/g" "${ROOT_DIR}/first_run_gateway.sh"
+sed -i "s/%PI_INSTALL_DOCKER%/$PI_INSTALL_DOCKER/g" "${ROOT_DIR}/first_run_gateway.sh"
+sed -i "s/%PI_GPU_MEMORY%/$PI_GPU_MEMORY/g" "${ROOT_DIR}/first_run_gateway.sh"
+sed -i "s/%PI_IP_ADDRESS_RANGE_START%/$PI_IP_ADDRESS_RANGE_START/g" "${ROOT_DIR}/first_run_gateway.sh"
+sed -i "s/%PI_IP_ADDRESS_RANGE_END%/$PI_IP_ADDRESS_RANGE_END/g" "${ROOT_DIR}/first_run_gateway.sh"
+sed -i "s/%PI_DNS_ADDRESS%/$PI_DNS_ADDRESS/g" "${ROOT_DIR}/first_run_gateway.sh"
+
+chmod 755 "${ROOT_DIR}/first_run_gateway.sh"
+cp "${PI_SSH_KEY}" "${ROOT_DIR}/id_rsa.pub"
+cp -Rf ./data "${ROOT_DIR}/opt/data"
+#cp -Rf ./scripts/interfaces.py "${ROOT_DIR}/interfaces.py"
+#cp ./files/hosts "${ROOT_DIR}/etc/hosts"
+
+# Set the crontab to update on a daily basis
+#cp ./files/update.sh "${ROOT_DIR}/etc/cron.daily/update"
+#chmod 755 "${ROOT_DIR}/etc/cron.daily/update"
+
+rm "${ROOT_DIR}/etc/motd"
+cp ./files/motd.sh "${ROOT_DIR}/etc/profile.d/motd.sh"
+chmod 755 "${ROOT_DIR}/etc/profile.d/motd.sh"
+
+# Clean up after ourselves
+umount ${BOOT_DIR} || true
+umount ${ROOT_DIR} || true
+
+kpartx -d ${UNZIP_TARGET}
+
+echo "username: ${PI_USERNAME}" >> "${RESULT_FILE}"
+echo "password: ${PI_PASSWORD}" >> "${RESULT_FILE}"
+
+echo "--- Finished successfully ---"
+echo "username: ${PI_USERNAME}"
+echo "password: ${PI_PASSWORD}"
+echo "-----------------------------"
